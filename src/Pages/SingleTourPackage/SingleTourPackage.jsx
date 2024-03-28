a30943c592e48b8aba7332ff068620e6367455b0
Maruf Siyam <marufsiyam9123@gmail.com>
2024-03-28 10:41:35 +0600

    pagination added

--- src/Pages/SingleTourPackage/SingleTourPackage.jsx
+++ src/Pages/SingleTourPackage/SingleTourPackage.jsx
@@ -5,6 +5,8 @@ import { useParams } from "react-router-dom";
 import { MdOutlineStarHalf } from "react-icons/md";
 import styles from "./SingleTourPackage.module.css";
 import { CiLocationOn } from "react-icons/ci";
+import { IoCheckmarkOutline } from "react-icons/io5";
+import { RxCross1 } from "react-icons/rx";
 
 const SingleTourPackage = () => {
   const [tourData, setTourData] = useState(null);
@@ -68,8 +70,34 @@ const SingleTourPackage = () => {
                 and haircare are 100% and 104% more expensive, respectively.`
               </p>
             </div>
-            <div className={styles.included_excluded}>
+            <div className={styles.included_excluded_wrapper}>
               <h3>Included and Excluded</h3>
+              <div className={styles.included_excluded}>
+              <div className={styles.included}>
+                <p> <span><IoCheckmarkOutline /></span> Meal as per hotel Plan and drinks free too.</p>
+                <p> <span><IoCheckmarkOutline /></span> Return airport and round trip transfers.</p>
+                <p> <span><IoCheckmarkOutline /></span> Accommodation on twin sharing basis.</p>
+                <p> <span><IoCheckmarkOutline /></span> The above rates are on per day disposal basis.</p>
+                <p> <span><IoCheckmarkOutline /></span> Enjoy Brussels day tours. Overnight Brussels</p>
+              </div>
+              <div className={styles.excluded}>
+                <p> <span><RxCross1 /></span> AC will not be functional on Hills or Slopes.</p>
+                <p> <span><RxCross1 /></span> Any other service not mentioned</p>
+                <p> <span><RxCross1 /></span> Additional entry fees other than specified</p>
+                <p> <span><RxCross1 /></span> Amsterdam canal cruise not included for basic</p>
+              </div>
+              </div>
+              <div className={styles.highlight_wrapper}>
+                <h3>Top Highlights</h3>
+                <p>Komodo Island is one of the only places in the world where you can spot Komodo Dragons the world’s largest of lizards in their natural environment. islands of the Komodo National Park.</p>
+                <div className={styles.highlight}>
+                  <p>Easily generate images with detailed text prompts</p>
+                  <p>Customize your images with different styles, moods, colors</p>
+                  <p>Generate high-quality images with up to 8K resolution</p>
+                  <p>Customize your images with different styles, moods, colors</p>
+                  <p>Generate high-quality images with up to 8K resolution</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
--- src/Pages/TourList/TourList.jsx
+++ src/Pages/TourList/TourList.jsx
@@ -7,12 +7,14 @@ import TourListSearchbar from "../../Components/TourList_Searchbar/TourListSearc
 
 const TourList = () => {
   const [tours, setTours] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(6)
 
   useEffect(() => {
-    fetch("https://travelbooking-2ufk.onrender.com/tours")
-      .then((res) => res.json())
-      .then((data) => setTours(data));
-  }, []);
+    fetch(`https://travelbooking-2ufk.onrender.com/tours?page=${currentPage}&pageSize${pageSize}`)
+    .then((res) => res.json())
+    .then((data) => setTours(data.tours)); 
+}, [currentPage, pageSize]);
 
     const fetchTours = async (sortBy) => {
       const response = await fetch(`https://travelbooking-2ufk.onrender.com/tours?region=${sortBy}`);
@@ -43,6 +45,15 @@ const TourList = () => {
             ))}
           </div>
         </div>
+      </div>
+      <div className={styles.pagination_controls}>
+        <button style={{display: currentPage === 1 ? 'none' : 'block'}} onClick={() => setCurrentPage((prevState) => prevState - 1)}  >
+          Previous
+        </button>
+        <span>Current Page: {currentPage}</span>
+        <button style={{display: currentPage === 3 ? 'none' : 'block'}} onClick={() => setCurrentPage((prevState) => prevState + 1)} >
+          Next
+        </button>
       </div>
     </div>
   );
